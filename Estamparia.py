7ec9be8b567792c1415b595fbcf94664d86b316b
PabloJorge99 <pabloro99@hotmail.com>
2025-05-08 19:22:44 -0300

    Delete Estamparia.py

--- Estamparia.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Estamparia")
-    st.write("bem vindo a estamparia aqui voce acompanha o que estamos produzindo")
-    st.markdown("---")
-
-if __name__ == "__main__":
-    main()
