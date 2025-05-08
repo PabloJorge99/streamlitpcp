d654a3f6ea19764a8824bd0aaac3d92f19e44980
PabloJorge99 <pabloro99@hotmail.com>
2025-05-08 20:39:12 -0300

    Delete Pedidos.py

--- Pedidos.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Pedidos Maquinas")
-    st.markdown("---")
-    st.write("adijwefiejfuiefijeiofiejfioeiojeiojfijeifji9ewjfiejwfijewifjiew9fji9ewjfi9ewj")
-if __name__ == "__main__":
-    main()
